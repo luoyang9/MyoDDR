b53436b18e82d919312f9b1826e9ea34801faac9
Charlie Zhang <peidong97@yahoo.ca>
2015-11-15 02:02:16 -0500

    some polish, bug fixes

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var count;
 (function(){
 	var app = angular.module('personal', []);
 
-	app.controller("GameController", function($scope, $interval){
+	app.controller("GameController", function($scope){
 		$scope.score = 0;
 
 		$scope.createNote = function(noteID){
@@ -25,17 +25,22 @@ var count;
 		};
 
 
-		$scope.loadSong = function(){
-			//var songDelay = 800;
-			//var delay = 73.5;
-			//var notes = [1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,1,2,4,3,1,3,3,3,1,3,3,1,3,1,3,3,1,3,3,1,3,1,4,4,1,4,4,1,4,1,4,4,1,4,4,1,4,1,3,3,1,3,3,1,3,1,3,3,1,3,3,1,3,1,2,2,1,2,2,1,2,1,4,2,1,2,4,3,1,3,3,3,1,3,3,1,3,1,3,3,1,3,3,1,3,1,4,4,1,4,4,1,4,1,4,4,1,4,4,1,4,1,3,3,1,3,3,1,3,1,3,3,1,3,3,1,3,1,3,1,2,3,4,2,3,4,4,3,4,2,3,1,2,2,2,2,2,3,3,4,3,1,3,4];
-			//var noteDelays = [0, delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay*8,delay*16,0,delay*16,0,delay*16,0,delay*2,delay*2,delay,delay*2,delay*2,delay,delay,delay,delay,delay];
+		$scope.loadSong = function(songURL){
 
-			var songDelay = 0;
-			var notes = [3,2,4,3,2,1,3,2,4,3,2,1,3,2,4,3,2,1,3,2,4,3,2,1,2,1,3,2,3,4,4,3,4,3,4,2,4,4,2,4,2,4,3,2,3,4,3];
-			var noteDelays = [4000, 250, 550, 250, 550, 250, 1000,250, 550, 250, 550, 250, 1000, 250, 550, 250, 550, 250, 1000, 250, 550, 250, 550, 250, 1000, 250, 550, 250, 550, 250, 6300, 110, 110, 110, 110, 110, 210, 210, 210, 110, 210, 210, 210, 210, 110, 110, 110];
-			console.log(notes.length + " " + noteDelays.length);
-			count = 0;
+			if(songURL == "fire"){
+				document.getElementById("currentSong").src = "music/throughthefireandflames.mp3";
+				var songDelay = 800;
+				var delay = 74;
+				var notes = [1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,3,1,4,2,3,1,1,2,3,1,2,3,4,2,4,2,1,2,4,3,1,3,3,3,1,3,3,1,3,1,3,3,1,3,3,1,3,1,4,4,1,4,4,1,4,1,4,4,1,4,4,1,4,1,3,3,1,3,3,1,3,1,3,3,1,3,3,1,3,1,2,2,1,2,2,1,2,1,4,2,1,2,4,3,1,3,3,3,1,3,3,1,3,1,3,3,1,3,3,1,3,1,4,4,1,4,4,1,4,1,4,4,1,4,4,1,4,1,3,3,1,3,3,1,3,1,3,3,1,3,3,1,3,1,3,1,2,3,4,2,3,4,4,3,4,2,3,1,2,2,2,2,2,3,3,4,3,1,3,4];
+				var noteDelays = [0, delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay,delay*8,delay*16,0,delay*16,0,delay*16,0,delay*2,delay*2,delay,delay*2,delay*2,delay,delay,delay,delay,delay];
+			}else{
+				var songDelay = 0;
+				var notes = [3,2,4,3,2,1,3,2,4,3,2,1,3,2,4,3,2,1,3,2,4,3,2,1,2,1,3,2,3,4,4,3,4,3,4,2,4,4,2,4,2,4,3,2,3,4,3,4,3,4,3,4,2,4,4,3,4,2,4,3,2,3,4,3, 3,2,4,3,2,1,3,3,3,4,3,3,3,3,4,3,2,4,3,2,1,3,3,3,4,3,3,3,3,4,3,2,4,3,2,1,3,3,3,4,3,3,3,3,4,2,1,3,2,3,4,2,3,3,2,3,3,3,2,3,4,4,4,3,2,3,3,2,3,3,3,3,3,2,3,4,4,3,2,2,3,3,2,3,3,3,2,3,4,4,3,2,2,4,4,3,2,4,4,3,2,2,3,3,2,3,2,3,3,2,1,2];
+				var noteDelays = [4000, 250, 550, 250, 550, 250, 1000,250, 550, 250, 550, 250, 1000, 250, 550, 250, 550, 250, 1000, 250, 550, 250, 550, 250, 1000, 250, 550, 250, 550, 250, 6300, 110, 110, 110, 110, 110, 210, 210, 210, 110, 210, 210, 210, 210, 110, 110, 110, 840, 110, 110, 110, 110, 110, 210, 210, 210, 110, 210, 210, 210, 210, 110, 110, 110, 3150, 150, 240, 150, 240, 150, 260, 100, 100, 100, 300, 100, 100, 100, 100, 300, 150, 240, 150, 240, 150, 260, 100, 100, 100, 300, 100, 100, 100, 100, 300, 150, 240, 150, 240, 150, 260, 100, 100, 100, 300, 100, 100, 100, 100, 300, 150, 240, 150, 240, 240, 3610, 100, 100, 100, 100, 500, 100, 100, 100, 500, 100, 100, 200, 1300, 100, 100, 100, 100, 400, 100, 100, 100, 100, 400, 100, 100, 100, 100, 1300, 100, 100, 100, 100, 500, 100, 100, 120, 500, 100, 100, 100, 1300, 100, 100, 110, 110, 550, 100, 100, 110, 400, 100, 100, 100, 100, 400, 100, 100, 100, 200, 1010];		
+			}
+
+			console.log(notes.length + " " + noteDelays.length);	
+		 	count = 0;
 
 			this.createSong(notes, noteDelays);
 
--- js/main.js
+++ js/main.js
@@ -27,12 +27,40 @@ function callCreateSong(notes, noteDelays){
 
 Myo.connect('com.enghack.poseDetect');
 
+
+function yawSync(){
+	setTimeout(function(){
+		Myo.myos[0].zeroOrientation();
+		Myo.myos[1].zeroOrientation();
+		console.log('Synced');
+	}, 1000);
+}
+
 Myo.on('connected', function(){
 	Myo.setLockingPolicy('none');
 
 	var directionL = 'middle', directionR = 'middle';
 	var poseL = 'open', poseR = 'open';
 	var pitch, yaw;
+
+
+	function getDir(p, y){
+		if(-0.5 < p && p < 0.5){
+			if(y > 0.5){
+				return 'left';
+			}else if(y < -0.5){
+				return 'right';
+			}else{
+				return 'middle';
+			}
+		}else{
+			if(p > 0.5){
+				return 'up';
+			}else{
+				return 'down';
+			}
+		}
+	}
 
 	function getDir(p, y){
 		if(-0.5 < p && p < 0.5){
@@ -79,7 +107,6 @@ Myo.on('connected', function(){
 	});
 
 	setInterval(function(){
-			console.log(directionR + " " + directionL);
 		if(previousActionR != directionR)
 		{
 		  previousActionR = directionR;
@@ -99,3 +126,87 @@ Myo.on('connected', function(){
 	}, 0);
 });
 
+
+
+ function CSVToArray( strData, strDelimiter ){
+        // Check to see if the delimiter is defined. If not,
+        // then default to comma.
+        strDelimiter = (strDelimiter || ",");
+
+        // Create a regular expression to parse the CSV values.
+        var objPattern = new RegExp(
+            (
+                // Delimiters.
+                "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+                // Quoted fields.
+                "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+                // Standard fields.
+                "([^\"\\" + strDelimiter + "\\r\\n]*))"
+            ),
+            "gi"
+            );
+
+
+        // Create an array to hold our data. Give the array
+        // a default empty first row.
+        var arrData = [[]];
+
+        // Create an array to hold our individual pattern
+        // matching groups.
+        var arrMatches = null;
+
+
+        // Keep looping over the regular expression matches
+        // until we can no longer find a match.
+        while (arrMatches = objPattern.exec( strData )){
+
+            // Get the delimiter that was found.
+            var strMatchedDelimiter = arrMatches[ 1 ];
+
+            // Check to see if the given delimiter has a length
+            // (is not the start of string) and if it matches
+            // field delimiter. If id does not, then we know
+            // that this delimiter is a row delimiter.
+            if (
+                strMatchedDelimiter.length &&
+                strMatchedDelimiter !== strDelimiter
+                ){
+
+                // Since we have reached a new row of data,
+                // add an empty row to our data array.
+                arrData.push( [] );
+
+            }
+
+            var strMatchedValue;
+
+            // Now that we have our delimiter out of the way,
+            // let's check to see which kind of value we
+            // captured (quoted or unquoted).
+            if (arrMatches[ 2 ]){
+
+                // We found a quoted value. When we capture
+                // this value, unescape any double quotes.
+                strMatchedValue = arrMatches[ 2 ].replace(
+                    new RegExp( "\"\"", "g" ),
+                    "\""
+                    );
+
+            } else {
+
+                // We found a non-quoted value.
+                strMatchedValue = arrMatches[ 3 ];
+
+            }
+
+
+            // Now that we have our value string, let's add
+            // it to the data array.
+            arrData[ arrData.length - 1 ].push( strMatchedValue );
+        }
+
+        // Return the parsed data.
+        return( arrData );
+    }
